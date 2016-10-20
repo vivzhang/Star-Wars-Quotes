9a055b1d46e299efbf2f00fbcec00020f178793b
vivian.mzh@gmail.com <vivian.mzh@gmail.com>
2016-10-20 13:30:47 -0700

    add main.js file, and event listener to the button, use fetch to fetch quotes page, send request once button is clicked and update the data

---
+++ public/main.js
@@ -0,0 +1,40 @@
+// excute the PUT request when the button is clicked
+
+var update = document.getElementById('update');
+
+update.addEventListener('click', function() {
+  // when the button is clicked, browser will send a PUT request thru fetch(fetch returns a Promise object) to our Express server, then the server responds by sending the changed quoted back to fetch(use .then method),
+
+  // The proper way to check if fetch resolved successfully is to use the ok method on the response object. You can then return res.json() if you want to read the data that was sent from the server:
+
+  // fetch({ /* request */ })
+  // .then(res => {
+  //   if (res.ok) return res.json();
+  // })
+  // .then(data => {
+  //   console.log(data);
+  // })
+
+  // window.fetch method to fetch recourses(a more powerful version of XMLHttpRequest)
+  // first para is the path
+  fetch('quotes', {
+    method: 'put',
+    headers: {'Content-Type': 'application/json'},
+    // convert body's content into JSON
+    body: JSON.stringify({
+      'name': 'Darth Vader',
+      'quote': 'I find you lack of faith disturbing.'
+    })
+  }).then(res => {
+    if (res.ok) return res.json();
+  })
+  .then(data => {
+    console.log(data);
+    // update the DOM so the user can see the change immediately
+    window.location.reload(true);
+  })
+})
+
+
+
+
